bdcfbd57c12fbe1fc438bb99b89468603e48beeb
snowpeech <snowpeech@gmail.com>
2019-03-25 21:49:01 -0500

    it works

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
 let toDo = [];
+var itemID=0;
+let showTasks = document.querySelector("#display");
 
 function add () {
-	let task = document.querySelector('#inTask').value
+	let task = document.querySelector('#inTask'); 
+	itemID++;
+		//can make task = #inTask.value or #inTask, and leave .value to the push fn
+
+	toDo.push({ "tasko"	: task.value, "id":itemID});
+
+	console.log(toDo);
+
+	refreshList();
 }
+
+function refreshList(){
+	showTasks.innerHTML = '';
+
+	for (var i = 0; i < toDo.length; i++) {
+		showTasks.innerHTML = showTasks.innerHTML + "<BR>" + toDo[i].tasko + `<button class="kill" onclick="remove(${i})">X</button>` 
+	}
+
+	document.querySelector("#inTask").value ="";
+}
+
+function remove(index) {
+
+    toDo.splice(index, 1);
+
+    refreshList();
+
+ }
